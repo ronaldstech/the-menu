b7cc87fef19e4a54c3b9794edac22bff77223b98
ronaldstech <liwewejacob265@gmail.com>
2026-09-26 20:19:36 +0200

    production url backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const configuredBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://apexspacemw.com/rt/the_menu'
+const configuredBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://apexspacemw.com/rt/the_menu_backend'
 
 export const API_BASE_URL = configuredBaseUrl.replace(/\/+$/, '')
 
